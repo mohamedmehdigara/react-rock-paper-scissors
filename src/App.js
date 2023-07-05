8ae0555a6b4f09bd785719bb4b6e56e315b3e3d4
mohamedmehdigara <garamehdi57@yahoo.com>
2023-07-05 10:08:48 +0100

    added files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const choices = ['rock', 'paper', 'scissors'];
+
+const App = () => {
+  const [playerChoice, setPlayerChoice] = useState(null);
+  const [computerChoice, setComputerChoice] = useState(null);
+  const [result, setResult] = useState(null);
+
+  const handleChoice = (choice) => {
+    const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+    setPlayerChoice(choice);
+    setComputerChoice(computerChoice);
+    setResult(getResult(choice, computerChoice));
+  };
+
+  const getResult = (playerChoice, computerChoice) => {
+    if (playerChoice === computerChoice) {
+      return "It's a tie!";
+    } else if (
+      (playerChoice === 'rock' && computerChoice === 'scissors') ||
+      (playerChoice === 'paper' && computerChoice === 'rock') ||
+      (playerChoice === 'scissors' && computerChoice === 'paper')
+    ) {
+      return 'You win!';
+    } else {
+      return 'Computer wins!';
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Rock, Paper, Scissors</h1>
+      <div className="choices">
+        {choices.map((choice) => (
+          <button key={choice} onClick={() => handleChoice(choice)}>
+            {choice}
+          </button>
+        ))}
+      </div>
+      {playerChoice && (
+        <div className="result">
+          <p>You chose: {playerChoice}</p>
+          <p>Computer chose: {computerChoice}</p>
+          <p>{result}</p>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
